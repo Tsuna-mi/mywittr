9ac658b114e61d94599595473d5e4bd740e94386
Ana Gamito <ana.gamito.pasiona@axa-groupsolutions.com>
2017-12-09 17:29:18 +0100

    fetch new response with headers content-type

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,11 @@
 self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
+  // Ensure the Content-Type of the response is "text/html"
+  event.respondWith(
+    new Response ('Hello <b class="a-winner-is-me">world</b>', {
+      headers: {'Content-Type': 'text/html'}
+    }))
+
   console.log(event.request);
 });
