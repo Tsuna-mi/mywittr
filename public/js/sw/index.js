1b2d7ade8177be6d88e9cb6dfb3e75718a4a016b
Ana Gamito <ana.gamito.pasiona@axa-groupsolutions.com>
2017-12-09 17:57:13 +0100

    Deal with 404 status

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,10 +1,22 @@
+// self.addEventListener('fetch', function(event) {
+//   // TODO: only respond to requests with a
+//   // url ending in ".jpg"
+//   console.log(event.request);
+//   if (event.request.url.endsWith('.jpg')) {
+//     event.respondWith(
+//       fetch('/imgs/dr-evil.gif')
+//     );
+//   };
+// });
 self.addEventListener('fetch', function(event) {
-  // TODO: only respond to requests with a
-  // url ending in ".jpg"
-  console.log(event.request);
-  if (event.request.url.endsWith('.jpg')) {
-    event.respondWith(
-      fetch('/imgs/dr-evil.gif')
-    );
-  };
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      if (response.status == 404) {
+        return new Response ("Whoops, not found");
+      }
+      return response;
+    }).catch(function(){
+      return new Response ("Uh oh, that totally failed!");
+    })
+  );
 });
