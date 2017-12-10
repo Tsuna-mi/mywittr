6c5261dd3ed31c3266b1eee1cb669fec1861fd74
Ana Gamito <ana.gamito.pasiona@axa-groupsolutions.com>
2017-12-10 18:52:03 +0100

    update css on shift-reload deleting old cache

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(event) {
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v1').then(function(cache) {
+    caches.open('wittr-static-v2').then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -17,10 +17,23 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
+  console.log('sw activated');
   event.waitUntil(
-    // TODO: remove the old cache
-  );
-});
+
+      caches.keys().then(function(cacheNames) {
+        console.log(cacheNames);
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName === 'wittr-static-v1' ) {
+              console.log("[Service Worker] Removing Old Caches.", cacheName);
+              return caches.delete(cacheName);
+            }
+            console.log(cacheNames);
+          })
+        );
+      })
+    );
+  });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
