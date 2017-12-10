ab83c9005bf80f80c59d289c258e401d8934a184
Ana Gamito <ana.gamito.pasiona@axa-groupsolutions.com>
2017-12-10 17:44:52 +0100

    Cache served if no network - 2

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -19,11 +19,8 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request)
-  .then((response) => {
-    if (!response) {
-     return fetch(event.request);
-    } else {
-     return response;
-    }
-  })
-)});
+  .then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
